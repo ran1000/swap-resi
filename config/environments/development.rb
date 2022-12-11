b930394502b6aee6fdd579efe8414e8db03e9dc5
Ricardo Alexis Nader <ricardoalexis.nader@gmail.com>
2022-12-11 12:52:25 +0100

    devise password config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,12 +37,28 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  # config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.perform_caching = false
 
   # Added with Devise
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  # Added for Reset Password
+  config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_options = {from: 'salvovega21@gmail.com'}
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:             'salvovega21@gmail.com',
+    port:                587,
+    domain:              'gmail.com',
+    user_name:           Rails.application.credentials.gmail[:email],
+    password:            Rails.application.credentials.gmail[:password],
+    authentication:      'plain',
+    enable_starttls_auto: true
+  }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
