45889e2d5319920775ed33700c466fe51b70b301
Ricardo Alexis Nader <ricardoalexis.nader@gmail.com>
2023-01-12 11:19:53 +0100

    adjusted index cards design

--- app/models/space.rb
+++ app/models/space.rb
@@ -2,6 +2,6 @@ class Space < ApplicationRecord
   belongs_to :user
   has_many_attached :photos
 
-  validates :name, presence: true, uniqueness: true, length: { minimum: 6 }
+  validates :name, presence: true, uniqueness: true, length: { minimum: 6, maximum: 70 }
   validates :daily_cost, presence: true, numericality: true, inclusion: { in: 3..20 , wrong_length: "Value should be between 3 and 20" }
 end
