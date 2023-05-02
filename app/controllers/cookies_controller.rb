9c331f43bb7913dee4510b31bc8e332123684529
Ricardo Alexis Nader <ricardoalexis.nader@gmail.com>
2023-05-02 16:52:52 +0200

    fix bug session value

--- app/controllers/cookies_controller.rb
+++ app/controllers/cookies_controller.rb
@@ -1,12 +1,13 @@
 class CookiesController < ApplicationController
   skip_before_action :authenticate_user!, only: %i[index consent policy]
   def index
-
   end
 
   def consent
-    session[:cookie_consent] = params[:consent].presence
-    render turbo_stream: turbo_stream.remove(:cookie_consent)
+    session[:cookie_consent] = params[:consent]
+    if session[:cookie_consent] == "true"
+      render turbo_stream: turbo_stream.remove(:cookie_consent)
+    end
   end
 
   def policy
