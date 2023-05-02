dd5db4a7a1c21572193ed5ac94ab01332d02b917
Ricardo Alexis Nader <ricardoalexis.nader@gmail.com>
2023-05-02 14:30:31 +0200

    set authentication and authorization for cookies

--- app/controllers/cookies_controller.rb
+++ app/controllers/cookies_controller.rb
@@ -1,11 +1,13 @@
 class CookiesController < ApplicationController
+  skip_before_action :authenticate_user!, only: %i[index consent policy]
   def index
+
   end
 
   def consent
-    # 01 Ask User for consent and store in a variable
-    # 02 Track Everything if consnt is true
-    # 03 Remove cookie banner
+    # 1. Ask User for consent and store in a variable
+    # 2. Track Everything if consnt is true
+    # 3. Remove cookie banner
   end
 
   def policy
