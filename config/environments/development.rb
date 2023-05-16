9f49d892b0b3dd107717ffcfbd715671848c93fa
Ricardo Alexis Nader <ricardoalexis.nader@gmail.com>
2023-05-16 17:55:34 +0200

    style request card

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -86,4 +86,7 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  # Compiles assets on-the-fly, avoiding to use production precompiled assets (clobber)
+  config.assets.prefix = "/dev-assets"
 end
